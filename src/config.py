65f1e5119999d42714a6580a1ceef348a08ebe70
SkyShadex <shadexh2o@hotmail.com>
2024-04-30 01:22:25 +0000

    Added keys for dev and prod

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-import os, app
+import os, main
 from dotenv import find_dotenv, load_dotenv
 
 # Dev Env Variables
@@ -9,6 +9,8 @@ load_dotenv(dotenv_path)
 # Security
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
+API_KEY_PROD = os.getenv("API_KEY_0")
+API_SECRET_PROD = os.getenv("API_SECRET_0")
 WEBHOOK_PASSPHRASE = os.getenv("WEBHOOK_PASSPHRASE")
 
 # Social Hooks
@@ -21,13 +23,13 @@ RISK = 1 # Unit risked
 SLIPPAGE = 1.5
 RISK_EXPOSURE = 0.05 #equal weights divided by the number of actively traded symbols
 TAKEPROFIT_POSITION = 0.5
-DAYTRADE_ALLOW = True
+DAYTRADE_ALLOW = True 
 FRACTIONAL_ALLOW = True
 MARGIN_ALLOW = True
 EXTENDTRADE_ALLOW = False
 
 # Portfolio Level Strategy
-PORTFOLIO_REBAL = 0.6
+PORTFOLIO_REBAL = 0.55
 
 # Network Settings
 LOCAL_HOST = '0.0.0.0'
