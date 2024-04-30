24ca2d51faba7db735034f0a02b7ae0fcfdb1d36
SkyShadex <shadexh2o@hotmail.com>
2024-04-30 01:23:00 +0000

    using less workers and more background threads

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -2,6 +2,7 @@
 import multiprocessing
 
 bind = '0.0.0.0:5000'  # Bind to all available network interfaces to port
-workers = min(multiprocessing.cpu_count() * 2 + 1,10)
+workers = 1#min(multiprocessing.cpu_count() * 2 + 1,10)
+print(f'initializing {workers} workers')
 
-#gunicorn -c gunicorn_config.py app:app
+#gunicorn -c gunicorn_config.py main:app
