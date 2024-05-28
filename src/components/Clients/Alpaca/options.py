4587875f7e122da9da70c0a2444db2919c611786
SkyShadex <shadexh2o@hotmail.com>
2024-05-28 15:15:15 +0000

    convert alpaca options chain response to df

--- src/components/Clients/Alpaca/options.py
+++ src/components/Clients/Alpaca/options.py
@@ -14,7 +14,7 @@ from components.Clients.Alpaca.price_data import get_ohlc_alpaca
 from sklearn.preprocessing import MinMaxScaler
 from alpaca.data.historical.option import OptionHistoricalDataClient
 from alpaca.data.requests import OptionChainRequest
-
+import logging
 import requests
 import json
 import pandas as pd
@@ -36,56 +36,145 @@ class AlpacaOptionContracts:
             "APCA-API-SECRET-KEY": self.secret
         } 
 
-    def parse_chain(self, underlying_symbols=None, status="active", expiration_date=None,
-                             expiration_date_gte=None, expiration_date_lte=None, root_symbol=None,
-                             option_type=None, style='american', strike_price_gte=None, strike_price_lte=None,
-                             page_token=None, limit=1000,override=False):
+    def parse_chain(self, underlying_symbol: str,
+                    strike_price_gte: float | None = None, 
+                    strike_price_lte: float | None = None,
+                    expiration_date: dt.date | str | None = None,
+                    expiration_date_gte: dt.date | str | None = None,
+                    expiration_date_lte: dt.date | str | None = None,
+                    root_symbol: str | None = None):
         
-        try:
-            params = OptionChainRequest(underlying_symbol=underlying_symbols,expiration_date_gte=expiration_date_gte,expiration_date_lte=expiration_date_lte)
-            optchain = apOpt.get_option_chain(request_params=params)
-            print(underlying_symbols,len(optchain))
-            optchain = pd.DataFrame(optchain)
-            print(optchain.iloc[:,:1])
-            # print(optchain.columns)
-            # print(optchain.iloc[-1])
-            # print(optchain.iloc[-5,-1])
-            # print(optchain.iloc[-4,-1])
-            # print(optchain.iloc[-3,-1])
-            # print(optchain.iloc[-2,-1])
-            # print(optchain.iloc[-1,-1])
-        except Exception as e:
-            print(e)
+            params = OptionChainRequest(underlying_symbol=underlying_symbol,expiration_date_gte=expiration_date_gte,expiration_date_lte=expiration_date_lte)
+            option_chain = apOpt.get_option_chain(request_params=params)
+            return option_chain
     
+    # def to_Dataframe(self,option_chain):
+    #     try:
+    #         chainDF = pd.DataFrame(option_chain).T
+    #         chainDF.columns=['symbol','latest_trade','latest_quote','implied_volatility','greeks']
+    #         chainDF.sort_index(inplace=True)
+    #         print(chainDF.shape)
+
+    #         def expand_column(df, col_name):
+    #             col_expanded = df[col_name].apply(lambda x: pd.Series(x, dtype=object))
+    #             if len(col_expanded.columns) > 1:
+    #                 for subcol in col_expanded.columns:
+    #                     nested_expanded = col_expanded[subcol].apply(lambda x: pd.Series(x, dtype=object))
+    #                     nested_expanded.columns = [f"{subcol}_{subcol2}" for subcol2 in nested_expanded.columns]
+    #                     col_expanded = col_expanded.drop(columns=[subcol]).join(nested_expanded)
+    #             col_expanded.columns = [f"{col_name}_{subcol}" for subcol in col_expanded.columns]
+    #             df = df.drop(columns=[col_name]).join(col_expanded)
+    #             return df
+
+    #         chainDF = expand_column(chainDF, 'symbol')
+    #         chainDF = expand_column(chainDF, 'latest_trade')
+    #         chainDF = expand_column(chainDF, 'latest_quote')
+    #         chainDF = expand_column(chainDF, 'implied_volatility')
+    #         chainDF = expand_column(chainDF, 'greeks')
+
+    #         for col in chainDF.columns:
+    #             if chainDF[col].apply(lambda x: isinstance(x, tuple)).any():
+    #                 nested_expanded = chainDF[col].apply(pd.Series)
+    #                 nested_expanded.columns = [f"{col}_{subcol}" for subcol in nested_expanded.columns]
+    #                 chainDF = chainDF.drop(columns=[col]).join(nested_expanded)
+
+    #         columns_to_drop = [col for col in chainDF.columns if str(chainDF[col][1]) in col]
+    #         chainDF = chainDF.drop(columns=columns_to_drop)
+
+    #         keywords = ['symbol', 'timestamp', 'exchange', 'price', 'size', 'id', 'conditions','tape']  # List of keywords to search for
+    #         for col in chainDF.columns:
+    #             if any(keyword in str(cell) for keyword in keywords for cell in chainDF[col]):
+    #                 chainDF.drop(columns=[col], inplace=True)
+
+    #         chainDF.columns = ['symbol','implied_volatility',0,'latest_trade_ts','exchange','price','size','id',1,2,3,'latest_quote_ts','ask_exchange','ask_price','ask_size','bid_exchange','bid_price','bid_size','conditions','tape',4,'delta',5,'gamma',6,'rho',7,'theta',8,'vega']
+            
+    #         columns_to_drop = [col for col in chainDF.columns if any(char.isdigit() for char in str(col))]
+    #         chainDF.drop(columns=columns_to_drop, inplace=True)
+    #         return chainDF        
+        # except Exception as e:
+        #     raise
+
+    def to_Dataframe(self,option_chain):
+        try:
+            chainDF = pd.DataFrame(option_chain).T
+            chainDF.columns=['symbol','latest_trade','latest_quote','implied_volatility','greeks']
+            chainDF.sort_index(inplace=True)
+
+            def expand_column(input_df, col_name):
+                df = input_df.copy()
+                columns_to_drop = [col for col in df.columns if col_name not in col]
+                df.drop(columns=columns_to_drop,inplace=True)
+                col_expanded = df[col_name].apply(lambda x: pd.Series(x, dtype=object))
+                if len(col_expanded.columns) > 1:
+                    for subcol in col_expanded.columns:
+                        nested_expanded = col_expanded[subcol].apply(lambda x: pd.Series(x, dtype=object))
+                        nested_expanded.columns = [f"{subcol}_{subcol2}" for subcol2 in nested_expanded.columns]
+                        col_expanded = col_expanded.drop(columns=[subcol]).join(nested_expanded)
+                col_expanded.columns = [f"{col_name}_{subcol}" for subcol in col_expanded.columns]
+                df = df.drop(columns=[col_name]).join(col_expanded)
+
+                for col in df.columns:
+                    if df[col].apply(lambda x: isinstance(x, tuple)).any():
+                        df2 = df[col].apply(pd.Series)
+                        df2.columns = [f"{col}_{subcol}" for subcol in df2.columns]
+                        df = df.drop(columns=[col]).join(df2)
+
+                if len(df.columns) > 2:
+                    df.drop(df.columns[[0]],axis=1,inplace=True)
+
+                column_labels = df.iloc[1,::2].copy()
+                output_df = df.iloc[:,1::2].copy() 
+                output_df.columns = column_labels.values
+
+                return output_df.copy()
+
+            symbol = expand_column(chainDF, 'symbol')
+            latest_trade = expand_column(chainDF, 'latest_trade')
+            latest_quote = expand_column(chainDF, 'latest_quote')
+            implied_volatility = expand_column(chainDF, 'implied_volatility')
+            greeks = expand_column(chainDF, 'greeks')
+
+            output_chain_df = symbol.join(latest_trade,rsuffix="_latest_trade").join(latest_quote,rsuffix="_latest_quote").join(implied_volatility,rsuffix="_implied_volatility").join(greeks,rsuffix="_greeks")
+            return output_chain_df.copy()
+        except Exception as e:
+            raise
+
     def get_option_contracts(self, underlying_symbols=None, status="active", expiration_date=None,
                              expiration_date_gte=None, expiration_date_lte=None, root_symbol=None,
                              option_type=None, style='american', strike_price_gte=None, strike_price_lte=None,
                              page_token=None, limit=1000,override=False):
+        
         # try:
-        #     self.parse_chain(underlying_symbols,expiration_date_gte,expiration_date_lte)
+        #     data=self.parse_chain(underlying_symbol=underlying_symbols,expiration_date_gte=expiration_date_gte,expiration_date_lte=expiration_date_lte)
+        #     data_df = self.to_Dataframe(data)
+        #     print(data_df.iloc[len(data_df)//2,:])
         # except Exception as e:
-        #     print(e) 
+        #     print(e)
 
-        params = {
-            "status": status,
-            "expiration_date": expiration_date,
-            "expiration_date_gte": expiration_date_gte,
-            "expiration_date_lte": expiration_date_lte,
-            "root_symbol": root_symbol,
-            "type": option_type,
-            "style": style,
-            "strike_price_gte": strike_price_gte,
-            "strike_price_lte": strike_price_lte,
-            "page_token": page_token,
-            "limit": limit
-        }
-        if underlying_symbols:
-            params["underlying_symbols"] = underlying_symbols
+        try:
+            params = {
+                "status": status,
+                "expiration_date": expiration_date,
+                "expiration_date_gte": expiration_date_gte,
+                "expiration_date_lte": expiration_date_lte,
+                "root_symbol": root_symbol,
+                "type": option_type,
+                "style": style,
+                "strike_price_gte": strike_price_gte,
+                "strike_price_lte": strike_price_lte,
+                "page_token": page_token,
+                "limit": limit
+            }
+            if underlying_symbols:
+                params["underlying_symbols"] = underlying_symbols
 
-        response = requests.get(self.url, headers=self.headers, params=params)
-        response.raise_for_status()  # Raise an exception for 4XX or 5XX status codes
-        return self.parse_response(response.json(),override)
-    
+            response = requests.get(self.url, headers=self.headers, params=params)
+            response.raise_for_status()  # Raise an exception for 4XX or 5XX status codes
+            return self.parse_response(response.json(),override)
+        
+        except (KeyError,ValueError,TypeError,Exception) as e:
+            raise   
+        
     def get_option_bars(self,symbol_or_symbols, timeframe, start=None, end=None, limit=None, sort='asc'):
         url = "https://data.alpaca.markets/v1beta1/options/bars"
 
@@ -105,10 +194,13 @@ class AlpacaOptionContracts:
         if need_fix:
             symbols = symbol_or_symbols[-1]
             single = True
+
         if multi:
             symbols = ','.join(symbol_or_symbols)
         elif single: 
             symbols = symbol_or_symbols   
+        else:
+            raise TypeError("invalid symbol input")
                     
         params = {
             "symbols": symbols,
@@ -166,9 +258,9 @@ class AlpacaOptionContracts:
         df['days_to_expiry'] = ((df['expiration_date'] - current_date).dt.days).round()
         df['days_to_expiry'] = df['days_to_expiry'].astype(int)
         try:
-            underlying = get_ohlc_alpaca(df['underlying_symbol'].iloc[-1],lookback=365,adjust="all",date_err=False)
+            underlying = get_ohlc_alpaca(symbols=df['underlying_symbol'].iloc[-1],lookback=365,adjust="all",date_err=False)
         except Exception as e:
-            raise RuntimeError(f"Missing Underlying Price Data for {df['underlying_symbol'].iloc[-1]}\n{e}")
+            raise Exception(f"[{AlpacaOptionContracts.__name__}] Error: Missing Underlying Price Data for {df['underlying_symbol'].iloc[-1]} : {e}")
         df['underlying_price'] = underlying['close'].iloc[-1]
         df['underlying_price_2'] = underlying['close'].iloc[-7]
         df['ul_vol'] = underlying['close'].pct_change().std()*np.sqrt(365)
